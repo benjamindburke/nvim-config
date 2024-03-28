7272adf31e7c15593e1d742b1c19ab622cb1fe95
Ben Burke <benjamindburke@gmail.com>
2024-03-28 11:03:56 -0400

    correct all tab chars to spaces

--- lua/nvim_config/packer.lua
+++ lua/nvim_config/packer.lua
@@ -20,47 +20,46 @@ return require("packer").startup(function(use)
     -- Packer can manage itself
     use "wbthomason/packer.nvim"
 
-	use {
-		"nvim-telescope/telescope.nvim", tag = "0.1.6",
+    use {
+        "nvim-telescope/telescope.nvim", tag = "0.1.6",
         -- or                          , branch = "0.1.x",
-		requires = { {"nvim-lua/plenary.nvim"} }
-	}
+        requires = { {"nvim-lua/plenary.nvim"} }
+    }
 
-	-- color scheme
-	use { "catppuccin/nvim", as = "catppuccin" }
+    -- color scheme
+    use { "catppuccin/nvim", as = "catppuccin" }
 
-	-- treesitter parsing
-	use { "nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" } }
-	use "nvim-treesitter/playground"
+    -- treesitter parsing
+    use { "nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" } }
+    use "nvim-treesitter/playground"
 
-	-- harpoon file navigation
-	use "theprimeagen/harpoon"
+    -- harpoon file navigation
+    use "theprimeagen/harpoon"
 
-	-- tree navigation for vim undo branches
-	use "mbbill/undotree"
+    -- tree navigation for vim undo branches
+    use "mbbill/undotree"
 
-	-- git plugin
-	use "tpope/vim-fugitive"
+    -- git plugin
+    use "tpope/vim-fugitive"
 
-	use {
+    use {
         "vonheikemen/lsp-zero.nvim",
         branch = "v3.x",
         requires = {
-			-- LSP support
+            -- LSP support
             {"neovim/nvim-lspconfig"},
             {"williamboman/mason.nvim"},
-			{"williamboman/mason-lspconfig.nvim"},
+            {"williamboman/mason-lspconfig.nvim"},
 
             -- Autocompletion
             {"hrsh7th/nvim-cmp"},
             {"hrsh7th/cmp-nvim-lsp"},
-			{"hrsh7th/cmp-buffer"},
-			{"hrsh7th/cmp-path"},
-			{"saadparwaiz1/cmp_luasnip"},
-			{"hrsh7th/cmp-nvim-lua"},
-			{"L3MON4D3/LuaSnip"},
-			{"rafamadriz/friendly-snippets"}
+            {"hrsh7th/cmp-buffer"},
+            {"hrsh7th/cmp-path"},
+            {"saadparwaiz1/cmp_luasnip"},
+            {"hrsh7th/cmp-nvim-lua"},
+            {"L3MON4D3/LuaSnip"},
+            {"rafamadriz/friendly-snippets"}
         }
-	}
+    }
 end)
-
