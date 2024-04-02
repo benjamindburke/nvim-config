414f628f13b1c0cb4b8d8fb3dc993bada85a988b
Ben Burke <benjamindburke@gmail.com>
2024-04-02 19:35:12 -0400

    configure glow and watch plugins

---
+++ after/plugin/glow.lua
@@ -0,0 +1,2 @@
+-- render markdown (current buffer) in new vertical pane
+vim.keymap.set("n", "<leader>mg", vim.cmd.Glow)
---
+++ after/plugin/watch.lua
@@ -0,0 +1,2 @@
+-- open a new vertical pane and populate the :WatchStart command
+vim.keymap.set("n", "<leader>ws", [[:vnew<CR>:WatchStart ]])
