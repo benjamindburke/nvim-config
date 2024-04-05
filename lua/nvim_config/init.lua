da6e73ad89df8accda39879a239c6bbd8929cac9
Ben Burke <benjamindburke@gmail.com>
2024-04-05 00:07:58 -0400

    hardtime, text-to-colorscheme, indent-blankline, cellular-automaton installations

--- lua/nvim_config/init.lua
+++ lua/nvim_config/init.lua
@@ -1,2 +1,9 @@
 require("nvim_config.set")
 require("nvim_config.remap")
+
+require('text-to-colorscheme').setup {
+    ai = {
+        openai_api_key = os.getenv("OPENAI_API_KEY"),
+        gpt_model = "gpt-3.5-turbo-0613",
+    },
+}
