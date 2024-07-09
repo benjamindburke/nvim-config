8b6cf440b0d744d0a01e50995e36feb6f456bfc7
Ben Burke <benjamindburke@gmail.com>
2024-07-09 16:30:03 -0400

    update keybinds and fix lazy dep commands

--- lua/nvim-config/remap.lua
+++ lua/nvim-config/remap.lua
@@ -2,22 +2,31 @@
 -- vim motion remappings
 -----------------------------------
 
+-- create new vertical pane for a terminal
+vim.keymap.set("n", "<leader>tn", [[:100vne<CR>:term<CR><C-w>x<C-w>wi]])
+
+-- escape ends terminal insert mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
 -- file directory view
 vim.keymap.set("n", "<leader><leader>", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>dir", vim.cmd.Ex)
 
 -- in visual mode, select lines and then hold Shift+J to move lines up
-vim.keymap.set("v", "J", ":m '<-2<CR>gv=gv")
--- in visual mode, select lines and then hold Shift+K to move lines down
-vim.keymap.set("v", "K", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", ":", ":m '<-2<CR>gv=gv")
+-- in visual mode, select lines and then hold Shift+Y to move lines down
+vim.keymap.set("v", "I", ":m '>+1<CR>gv=gv")
 
 -- navigate between symbols - jump out of definition (pop tag from stack)
 vim.keymap.set("n", "<C-[>", "<C-RightMouse>")
 -- navigate between symbols - jump to definition (push tag to stack)
 vim.keymap.set("n", "<C-]>", "<C-]>")
 
+-- yank rest of line
 vim.keymap.set("n", "Y", "yg$")
+
+-- move the contents of line below cursor to end of current line
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- half page navigation - keep cursor at col 1
@@ -34,6 +43,7 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+-- delete text into empty register (i.e. do not yank)
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
 -- never press capital Q
@@ -62,29 +72,16 @@ vim.keymap.set("n", "<leader>f", function()
 end)
 
 -- quick fix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-i>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-;>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>i", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>;", "<cmd>lprev<CR>zz")
 
 -- regex replace
 vim.keymap.set("n", "<leader>s", [[:%s/\v\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- make a file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
--- navigate to next pane
-vim.keymap.set("n", "<leader>w<Right>", "<C-w>w")
--- navigate to previous pane
-vim.keymap.set("n", "<leader>w<Left>", "<C-w>W")
-
--- navigate to bottom right pane
-vim.keymap.set("n", "<leader>w<Down>", "<C-w>b")
--- navigate to top left pane
-vim.keymap.set("n", "<leader>w<Up>", "<C-w>t")
-
--- create new vertical pane for a terminal
-vim.keymap.set("n", "<leader>tn", [[:100vne<CR>:term<CR><C-w>x<C-w>wi]])
 
 -- open new vertical pane to this file
 vim.keymap.set("n", "<leader>remap", [[:sp ~/.config/nvim/lua/nvim-config/remap.lua<CR>]])
@@ -99,7 +96,10 @@ vim.keymap.set("n", "<leader>remap", [[:sp ~/.config/nvim/lua/nvim-config/remap.
 
 -- cellular-automaton.nvim
 -- make text on screen rain as if liquid
-vim.keymap.set("n", "<leader>rain", "<cmd>CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>rain", function()
+    require("cellular-automaton")
+    vim.cmd.CellularAutomaton("make_it_rain")
+end)
 
 -- duck.nvim
 -- create new duck at 2 steps a second
@@ -142,7 +142,10 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 -- glow.nvim
 -- render current buffer as markdown in new floating transparent pane
-vim.keymap.set("n", "<leader>md", vim.cmd.Glow)
+vim.keymap.set("n", "<leader>md", function()
+    require("glow")
+    vim.cmd.Glow()
+end)
 
 -- undotree.nvim
 -- open undo tree terminal ui
