f29a423e89f3545dcac6076e1d4050986db13f5e
Ben Burke <benjamindburke@gmail.com>
2024-11-05 16:35:59 -0500

    update hlchunk color for contrast

--- lua/plugins/hlchunk.lua
+++ lua/plugins/hlchunk.lua
@@ -1,12 +1,17 @@
 -- highlight current scope chunk to distinguish from indent-blankline highlights
+local style = {
+    "#2F78B4", -- blue
+    "#BA0000", -- maple red
+}
+
 return {
     "shellRaining/hlchunk.nvim",
     lazy  = true,
     event = { "BufReadPre", "BufNewFile", "InsertLeave" },
     opts  = {
         blank    = { enable = false },
-        chunk    = { enable = true, notify = false },
+        chunk    = { enable = true, notify = false, style = style },
         indent   = { enable = false },
-        line_num = { enable = true, notify = false },
+        line_num = { enable = true, notify = false, style = style },
     },
 }
